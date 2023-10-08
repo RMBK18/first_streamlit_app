ed61c391deca6352389bfa22894036a03f83556b
RMBK18 <147346597+RMBK18@users.noreply.github.com>
2023-10-08 18:50:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinnach & Rocket Smoothie')
+streamlit.text('Hard-Boiled free Range Egg')
+
+
